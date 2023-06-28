1680a4cbc3a0444b7cdfcd3bcdd272db1e3c3e35
tuna <1273146221@qq.com>
2023-06-28 20:52:34 +0800

    生物质液

---
+++ ET-Atiotus/minecraft/kubejs/server_scripts/immersiveengineering/squeezer.js
@@ -0,0 +1,17 @@
+onEvent('recipes', event => {
+
+
+    event.custom({"type":"immersiveengineering:squeezer","fluid":{"fluid":"kubejs:biomass","amount":100},"input":{"item":'kubejs:biomass_t0'},"energy":1000});
+    event.custom({"type":"immersiveengineering:squeezer","fluid":{"fluid":"kubejs:biomass","amount":200},"input":{"item":'kubejs:biomass_t1'},"energy":5000});
+    event.custom({"type":"immersiveengineering:squeezer","fluid":{"fluid":"kubejs:biomass","amount":400},"input":{"item":'kubejs:biomass_t2'},"energy":10000});
+    event.custom({"type":"immersiveengineering:squeezer","fluid":{"fluid":"kubejs:biomass","amount":800},"input":{"item":'kubejs:biomass_t3'},"energy":20000});
+    event.custom({"type":"immersiveengineering:squeezer","fluid":{"fluid":"kubejs:biomass","amount":2000},"input":{"item":'kubejs:biomass_t4'},"energy":40000});
+
+
+
+
+
+
+
+
+});
---
+++ ET-Atiotus/minecraft/kubejs/startup_scripts/fluid.js
@@ -0,0 +1,7 @@
+onEvent('fluid.registry', event => {
+    event.create("biomass")
+    .textureThick(0x808000)
+    .bucketColor(0x808000)
+    .displayName("biomass_fluid");
+
+});
